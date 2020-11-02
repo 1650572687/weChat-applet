4c0da3aabaf42162d899516c9c90e709d6229f5f
1650572687 <1650572687@qq.com>
2020-11-02 22:42:16 +0800

    第一次提交

---
+++ app.js
@@ -0,0 +1,39 @@
+//app.js
+App({
+  //onLaunch,onShow: options(path,query,scene,shareTicket,referrerInfo(appId,extraData))
+  onLaunch: function(options){
+    
+  },
+  onShow: function(options){
+
+  },
+  onHide: function(){
+
+  },
+  onError: function(msg){
+
+  },
+  //options(path,query,isEntryPage)
+  onPageNotFound: function(options){
+
+  },
+  globalData: {
+    
+  }
+});
+
+// ⾸⻚ index
+// 分类⻚⾯ category
+// 商品列表⻚⾯ goods_list
+// 商品详情⻚⾯ goods_detail
+// 购物⻋⻚⾯ cart
+// 收藏⻚⾯ collect
+// 订单⻚⾯ order
+// 搜索⻚⾯ search
+// 个⼈中⼼⻚⾯ user
+// 意⻅反馈⻚⾯ feedback
+// 登录⻚⾯ login
+// 授权⻚⾯ auth
+// 结算⻚⾯ pay
+
+
