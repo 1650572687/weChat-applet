7eae64c330d071dc205af0153774f3137e8b8caf
1650572687 <1650572687@qq.com>
2020-11-09 23:05:52 +0800

    商品分类页面提交

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,35 @@
 // pages/category/index.js
+import {request} from '../../request/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftTitle:[],//分类左边 产品名称栏目
+    rightConent:[],//右边详细信息
   },
+  cateList:[],//所有分类信息
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getCates(){
+    request({
+      url:'https://api-hmugo-web.itheima.net/api/public/v1/categories'
+    }).then(res => {
+      console.log('获取到的数据是',res)
+      this.cateList = res.data.message;
+      let leftTitle = this.cateList.map(v => v.cat_name) //左边数据
+      let rightConent = this.cateList[0].children
+      this.setData({
+        leftTitle,rightConent
+      })
+    })
   }
+
+  
 })
